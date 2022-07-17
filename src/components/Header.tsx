aa00b9647b001a289e8e39faa40e23dd7ee36695
Mike Kissinger <mis4mike@gmail.com>
2022-07-17 02:10:10 -0400

    more style tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 
+import vinniBackground from "../images/vinni-background.svg";
 import vinniJaw from "../images/vinni-jaw.svg";
 import vinniNoJaw from "../images/vinni-no-jaw.svg";
 
@@ -21,6 +22,7 @@ export const Header = () => {
           </p>
         </div>
         <div className="vinni-container">
+          <img className="vinni-background" src={vinniBackground} alt="" />
           <img className="vinni-head" src={vinniNoJaw} alt="" />
           <img className="vinni-jaw talking" src={vinniJaw} alt="" />
         </div>
