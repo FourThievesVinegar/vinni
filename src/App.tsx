81e8ef393617e6d9dc51ad06ebb01c52624814f5
Sparkles <sparklesthesparklyunicorn@gmail.com>
2024-08-19 12:19:58 -0400

    Add welcome dialog with fake pharma captcha

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ChemhackticaDialog } from "./components/Compounds/ChemhackticaDialog";
-import { ContextWrapper } from "./contexts";
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
-import { NewProject } from "./components/NewProject";
-import { Project } from "./components/Project";
-import { ProjectsList } from "./components/ProjectsList";
-import { ProjectsPage } from "./components/ProjectsPage";
-import { RecipeEditorDialog } from "./components/Recipes/RecipeEditingDialog";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ChemhackticaDialog } from './components/Compounds/ChemhackticaDialog'
+import { ContextWrapper } from './contexts'
+import { Header } from './components/Header'
+import { Home } from './components/Home'
+import { NewProject } from './components/NewProject'
+import { Project } from './components/Project'
+import { ProjectsList } from './components/ProjectsList'
+import { ProjectsPage } from './components/ProjectsPage'
+import { RecipeEditorDialog } from './components/Recipes/RecipeEditingDialog'
+import { WelcomeDialog } from './components/WelcomeDialog'
 
-import "./App.scss";
-import "./styles/Dialog.scss";
+import './App.scss'
+import './styles/Dialog.scss'
 
 function App() {
   return (
     <BrowserRouter>
       <ContextWrapper>
         <main>
+          <WelcomeDialog />
           <Header />
           <Routes>
             <Route index element={<Home />} />
@@ -31,7 +33,7 @@ function App() {
         </main>
       </ContextWrapper>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
